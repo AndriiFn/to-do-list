afa6f231fdd30088d05011fbc3891f41302acc26
Andrii Fedchuk <andriyfedchuk2006@gmail.com>
2025-03-01 14:46:02 +0100

    implement index function and TagListView class

--- task/views.py
+++ task/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from task.models import Task, Tag
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_tasks = Task.objects.count()
+    num_tags = Tag.objects.count()
+
+    context = {
+        "num_tasks": num_tasks,
+        "num_tags": num_tags,
+    }
+    return render(request, "task/index.html", context=context)
+
+
+class TagListView(ListView):
+    model = Tag
